c0f6b1da4592e87fb927d2299e81f0400aeef047
Anil Anar <anilanar@hotmail.com>
2023-01-09 08:00:25 +0100

    improve and document settings

--- src/settings.rs
+++ src/settings.rs
@@ -7,93 +7,99 @@ pub struct Settings {
     settings: HashMap<String, SettingsValue>,
 }
 
+/// Represents possible values a setting can take in settings lua files.
 #[derive(Debug)]
 pub enum SettingsValue {
     Bool(bool),
     Int(i64),
     Float(f64),
     String(String),
+    /// Represents non-standard utf8 strings which might contain FFXI specific
+    /// byte sequences that are only understood by the FFXI client.
     BadString(Vec<u8>),
 }
 
 impl Settings {
     pub fn new(lua: &Lua) -> Result<Settings, ServerError> {
-        let mut root_dir =
-            std::env::current_dir().map_err(ServerError::IOError)?;
-        root_dir.push("settings");
-        root_dir.push("default");
-
-        for entry in
-            std::fs::read_dir(&root_dir).map_err(ServerError::IOError)?
-        {
-            let entry = entry.map_err(ServerError::IOError)?;
-            let path = entry.path();
-            let is_lua =
-                path.extension().map(|ext| ext == "lua").unwrap_or(false);
-            if is_lua {
-                lua.execute_file(&path)?;
-            }
-        }
-
-        let table = lua
-            .globals()
-            .get::<_, mlua::Table>("xi")
-            .and_then(|table| table.get::<_, mlua::Table>("settings"))
-            .map_err(ServerError::LuaError)?;
-
-        let mut settings = HashMap::<String, SettingsValue>::new();
-
-        for outer_entry in table.pairs::<String, mlua::Table>() {
-            let (outer_key, outer_value) =
-                outer_entry.map_err(ServerError::LuaError)?;
-
-            for inner_entry in outer_value.pairs::<String, mlua::Value>() {
-                let (inner_key, inner_value) =
-                    inner_entry.map_err(ServerError::LuaError)?;
-
-                let value = match inner_value {
-                    mlua::Value::Boolean(bool) => {
-                        Some(SettingsValue::Bool(bool))
-                    }
-                    mlua::Value::Integer(n) => Some(SettingsValue::Int(n)),
-                    mlua::Value::Number(n) => Some(SettingsValue::Float(n)),
-                    mlua::Value::String(s) => {
-                        let bytes = s.as_bytes();
-                        let value = String::from_utf8(bytes.to_owned())
-                            .map(SettingsValue::String)
-                            .unwrap_or_else(|_| {
-                                SettingsValue::BadString(bytes.to_owned())
-                            });
-                        Some(value)
-                    }
-                    _ => None,
-                };
-
-                if let Some(value) = value {
-                    settings
-                        .insert(format!("{}.{}", outer_key, inner_key), value);
-                }
-            }
-        }
-
-        for entry in
-            std::fs::read_dir(&root_dir).map_err(ServerError::IOError)?
-        {
-            let entry = entry.map_err(ServerError::IOError)?;
-            let path = entry.path();
-            let is_lua =
-                path.extension().map(|ext| ext == "lua").unwrap_or(false);
-            if is_lua {
-                lua.execute_file(&path)?;
-            }
-        }
-
+        load_lua_from_dir(lua, "settings/default")?;
+        let settings = populate_hashmap(lua)?;
         Ok(Settings { settings })
     }
 
     pub fn get(self: &Settings, key: &str) -> Option<&SettingsValue> {
         self.settings.get(key)
     }
+}
+
+/// Reads all lua files in the given directory and loads them into `lua`, in
+/// whatever order OS reports them. Ignores non-lua files, if any.
+fn load_lua_from_dir<P: AsRef<std::path::Path>>(
+    lua: &Lua,
+    path: P,
+) -> Result<(), ServerError> {
+    let root = std::env::current_dir().map_err(ServerError::IOError)?;
+
+    for entry in
+        std::fs::read_dir(root.join(path)).map_err(ServerError::IOError)?
+    {
+        let entry = entry.map_err(ServerError::IOError)?;
+        let path = entry.path();
+        let is_lua = path.extension().map(|ext| ext == "lua").unwrap_or(false);
+        if is_lua {
+            lua.execute_file(&path)?;
+        }
+    }
+
+    Ok(())
+}
+
+/// Reads `xi.settings` from lua env, assumes a 2-level table hierarchy and
+/// populates a hash map.
+///
+/// For example, if `xi.settings.foo.bar = 5`, then the hash map will contain
+/// `("foo.bar", SettingsValue::Int(5))` pair.
+fn populate_hashmap(
+    lua: &Lua,
+) -> Result<HashMap<String, SettingsValue>, ServerError> {
+    let table = lua
+        .globals()
+        .get::<_, mlua::Table>("xi")
+        .and_then(|table| table.get::<_, mlua::Table>("settings"))
+        .map_err(ServerError::LuaError)?;
+
+    let mut settings = HashMap::<String, SettingsValue>::new();
+
+    for outer_entry in table.pairs::<String, mlua::Table>() {
+        let (outer_key, outer_value) =
+            outer_entry.map_err(ServerError::LuaError)?;
+
+        for inner_entry in outer_value.pairs::<String, mlua::Value>() {
+            let (inner_key, inner_value) =
+                inner_entry.map_err(ServerError::LuaError)?;
+
+            let value = match inner_value {
+                mlua::Value::Boolean(bool) => Some(SettingsValue::Bool(bool)),
+                mlua::Value::Integer(n) => Some(SettingsValue::Int(n)),
+                mlua::Value::Number(n) => Some(SettingsValue::Float(n)),
+                mlua::Value::String(s) => {
+                    let bytes = s.as_bytes();
+                    let value = String::from_utf8(bytes.to_owned())
+                        .map(SettingsValue::String)
+                        .unwrap_or_else(|_| {
+                            SettingsValue::BadString(bytes.to_owned())
+                        });
+                    Some(value)
+                }
+                _ => None,
+            };
+
+            if let Some(value) = value {
+                settings.insert(format!("{}.{}", outer_key, inner_key), value);
+            }
+        }
+    }
+
+    Ok(settings)
 }
 
 #[cfg(test)]
@@ -163,9 +169,7 @@ mod tests {
     fn it_loads_float_settings() {
         let lua = Lua::new().unwrap();
         let settings = Settings::new(&lua).unwrap();
-        let value = settings
-            .get("main.CASKET_DROP_RATE")
-            .unwrap();
+        let value = settings.get("main.CASKET_DROP_RATE").unwrap();
         assert_eq!(
             if let SettingsValue::Float(value) = value {
                 Some(value.to_owned())
